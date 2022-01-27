fbf5aa4dc0c66b6db31abb509a9a4873ed9da533
Sven <mrzunzi@rocketmail.com>
2022-01-28 00:04:21 +0100

    tests, pawns can take, better modularity

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,12 @@ impl Board {
         Board::from_fen(STARTING_POSITION)
     }
 
+    ///Readonly!
+    fn at(&self, rank: usize, file: usize) -> Square {
+        let (y, x) = Board::to_board(rank, file);
+        self.board[[y, x]]
+    }
+
     ///Only field 1 works at the moment (only position layout, with no further info)
     pub fn from_fen(fen: &str) -> Board {
         let mut board = Array2::<Square>::default((12, 10));
@@ -90,6 +96,8 @@ impl Board {
     ) {
         let (yf, xf) = Board::to_board(from_rank, from_file);
         let (yt, xt) = Board::to_board(to_rank, to_file);
+
+        //This check is redundant:
         let square = match self.board[[yf, xf]] {
             Square::Full(p) => Square::Full(p),
             _ => panic!(
@@ -97,7 +105,10 @@ impl Board {
                 from_rank, from_file
             ),
         };
-        if self.pseudo_legal_moves(yf, xf).unwrap().contains(&(yt, xt)) {
+        if self
+            .pseudo_legal_moves(from_rank, from_file, MoveType::Default)
+            .contains(&(yt, xt))
+        {
             eprintln!("allowed");
         } else {
             eprintln!("not allowed");
@@ -130,103 +141,195 @@ impl Board {
     ///
     /// todo: MOVES IN EIGENES STRUCT/FILE TUN
     /// -attacking move für pawn ist anders als normaler move
-    /// -soll nicht leeren vektor zurückgeben, wenn piece keine moves hat.
     /// -struktur bei pawns richtig schlecht
     /// -sliding pieces brauchen viel zu lange für den check
     /// -sliding pieces müssten eigentlich nur eine richtung berechnen, wenn ich
     /// den vorgeschlagenen move, der überprüft wird, auch beachten würde
-    pub fn pseudo_legal_moves(&self, y: usize, x: usize) -> Option<Vec<(usize, usize)>> {
+    ///
+    /// -bei attack squares für pawn ganz anders, für alle pieces darf der erste, wo ein
+    /// GEGENER draufsteht, auch angegriffen werden.
+    pub fn pseudo_legal_moves(
+        &self,
+        rank: usize,
+        file: usize,
+        move_type: MoveType,
+    ) -> Vec<(usize, usize)> {
+        let (y, x) = Board::to_board(rank, file);
         match self.board[[y, x]] {
-            Square::Full(p) => Some(match p.kind {
-                PieceKind::Pawn => match p.color {
-                    PieceColor::Black => match self.board[[y + 1, x]] {
-                        Square::Empty => {
-                            let mut v = vec![(y + 1, x)];
-                            if y == 3 {
-                                if let Square::Empty = self.board[[y + 2, x]] {
-                                    v.push((y + 2, x));
-                                }
+            Square::Full(p) => match p.kind {
+                PieceKind::Pawn => self.pseudo_legal_pawn_moves(p.color, y, x, move_type),
+                PieceKind::Knight => self.pseudo_legal_knight_moves(y, x, move_type),
+                PieceKind::King => self.pseudo_legal_king_moves(y, x, move_type),
+                PieceKind::Rook => self.pseudo_legal_rook_moves(y, x, move_type),
+                _ => Vec::new(),
+            },
+            Square::Empty => {
+                eprintln!(
+                    "Rank {}, file {} is empty, but pseudo legal moves inquired.",
+                    rank, file
+                );
+                Vec::new()
+            }
+            Square::Padding => {
+                panic!("valid rank & file gave a padding square")
+            }
+        }
+    }
+
+    fn pseudo_legal_pawn_moves(
+        &self,
+        color: PieceColor,
+        y: usize,
+        x: usize,
+        move_type: MoveType,
+    ) -> Vec<(usize, usize)> {
+        match color {
+            PieceColor::Black => match move_type {
+                MoveType::Default => match self.board[[y + 1, x]] {
+                    Square::Empty => {
+                        let mut v = vec![(y + 1, x)];
+                        if y == 3 {
+                            if let Square::Empty = self.board[[y + 2, x]] {
+                                v.push((y + 2, x));
                             }
-                            v
                         }
-                        _ => Vec::new(),
-                    },
-                    PieceColor::White => match self.board[[y - 1, x]] {
-                        Square::Empty => {
-                            let mut v = vec![(y - 1, x)];
-                            if y == 8 {
-                                if let Square::Empty = self.board[[y - 2, x]] {
-                                    v.push((y - 2, x));
-                                }
-                            }
-                            v
-                        }
-                        _ => Vec::new(),
-                    },
+                        v
+                    }
+                    _ => Vec::new(),
                 },
-                PieceKind::Knight => vec![
-                    (y - 2, x - 1),
-                    (y - 2, x + 1),
-                    (y - 1, x + 2),
-                    (y + 1, x + 2),
-                    (y + 2, x + 1),
-                    (y + 2, x - 1),
-                    (y + 1, x - 2),
-                    (y - 1, x - 2),
-                ],
-                PieceKind::King => vec![
-                    (y - 1, x - 1),
-                    (y - 1, x),
-                    (y - 1, x + 1),
-                    (y, x + 1),
-                    (y + 1, x + 1),
-                    (y + 1, x),
-                    (y + 1, x - 1),
-                    (y, x - 1),
-                ],
-                PieceKind::Rook => {
-                    //Upwards
+                MoveType::Attack => {
                     let mut v = Vec::new();
-                    for i in (2..y).rev() {
-                        if let Square::Empty = self.board[[i, x]] {
-                            v.push((i, x));
-                        } else {
-                            break;
+                    match self.board[[y + 1, x - 1]] {
+                        //Left attack
+                        Square::Full(p) if matches!(p.color, PieceColor::White) => {
+                            v.push((y + 1, x - 1));
                         }
+                        _ => (),
                     }
-                    //Downwards
-                    for i in y + 1..10 {
-                        println!("Now in {},{}", i, x);
-                        if let Square::Empty = self.board[[i, x]] {
-                            v.push((i, x));
-                        } else {
-                            break;
+                    match self.board[[y + 1, x + 1]] {
+                        //Right attack
+                        Square::Full(p) if matches!(p.color, PieceColor::White) => {
+                            v.push((y + 1, x + 1));
                         }
-                    }
-                    //Right
-                    for j in x + 1..9 {
-                        println!("Now in {},{}", y, j);
-                        if let Square::Empty = self.board[[y, j]] {
-                            v.push((y, j));
-                        } else {
-                            break;
-                        }
-                    }
-                    //Left
-                    for j in (1..x).rev() {
-                        println!("Now in {},{}", y, j);
-                        if let Square::Empty = self.board[[y, j]] {
-                            v.push((y, j));
-                        } else {
-                            break;
-                        }
+                        _ => (),
                     }
                     v
                 }
-                _ => Vec::new(),
-            }),
-            _ => None,
+            },
+            PieceColor::White => match move_type {
+                MoveType::Default => match self.board[[y - 1, x]] {
+                    Square::Empty => {
+                        let mut v = vec![(y - 1, x)];
+                        if y == 8 {
+                            if let Square::Empty = self.board[[y - 2, x]] {
+                                v.push((y - 2, x));
+                            }
+                        }
+                        v
+                    }
+                    _ => Vec::new(),
+                },
+                MoveType::Attack => {
+                    let mut v = Vec::new();
+                    match self.board[[y - 1, x - 1]] {
+                        //Left attack
+                        Square::Full(p) if matches!(p.color, PieceColor::Black) => {
+                            v.push((y - 1, x - 1));
+                        }
+                        _ => (),
+                    }
+                    match self.board[[y - 1, x + 1]] {
+                        //Right attack
+                        Square::Full(p) if matches!(p.color, PieceColor::Black) => {
+                            v.push((y - 1, x + 1));
+                        }
+                        _ => (),
+                    }
+                    v
+                }
+            },
         }
+    }
+
+    fn pseudo_legal_knight_moves(
+        &self,
+        y: usize,
+        x: usize,
+        move_type: MoveType,
+    ) -> Vec<(usize, usize)> {
+        vec![
+            (y - 2, x - 1),
+            (y - 2, x + 1),
+            (y - 1, x + 2),
+            (y + 1, x + 2),
+            (y + 2, x + 1),
+            (y + 2, x - 1),
+            (y + 1, x - 2),
+            (y - 1, x - 2),
+        ]
+    }
+
+    fn pseudo_legal_king_moves(
+        &self,
+        y: usize,
+        x: usize,
+        move_type: MoveType,
+    ) -> Vec<(usize, usize)> {
+        vec![
+            (y - 1, x - 1),
+            (y - 1, x),
+            (y - 1, x + 1),
+            (y, x + 1),
+            (y + 1, x + 1),
+            (y + 1, x),
+            (y + 1, x - 1),
+            (y, x - 1),
+        ]
+    }
+
+    fn pseudo_legal_rook_moves(
+        &self,
+        y: usize,
+        x: usize,
+        move_type: MoveType,
+    ) -> Vec<(usize, usize)> {
+        //Upwards
+        let mut v = Vec::new();
+        for i in (2..y).rev() {
+            if let Square::Empty = self.board[[i, x]] {
+                v.push((i, x));
+            } else {
+                break;
+            }
+        }
+        //Downwards
+        for i in y + 1..10 {
+            println!("Now in {},{}", i, x);
+            if let Square::Empty = self.board[[i, x]] {
+                v.push((i, x));
+            } else {
+                break;
+            }
+        }
+        //Right
+        for j in x + 1..9 {
+            println!("Now in {},{}", y, j);
+            if let Square::Empty = self.board[[y, j]] {
+                v.push((y, j));
+            } else {
+                break;
+            }
+        }
+        //Left
+        for j in (1..x).rev() {
+            println!("Now in {},{}", y, j);
+            if let Square::Empty = self.board[[y, j]] {
+                v.push((y, j));
+            } else {
+                break;
+            }
+        }
+        v
     }
 
     ///Example: Converts a to 1.
@@ -252,6 +355,8 @@ impl Board {
     }
 
     ///Draw chess board using unicode characters.
+    /// todo: could draw pseudolegal moves for a specific piece (maybe in
+    /// another function)
     pub fn draw(&self) {
         for rank in (1..9).rev() {
             for file in 1..9 {
@@ -381,6 +486,11 @@ pub enum Square {
     Padding,
 }
 
+pub enum MoveType {
+    Default,
+    Attack,
+}
+
 impl Square {
     fn unicode_str(&self) -> Option<&str> {
         match self {
@@ -396,6 +506,10 @@ impl Square {
             _ => None,
         }
     }
+
+    fn is_full(&self) -> bool {
+        matches!(*self, Self::Full(p))
+    }
 }
 
 impl Default for Square {
@@ -408,4 +522,76 @@ impl Default for Square {
 enum PieceColor {
     Black,
     White,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    //Tests currently ignore whose turn it is and whether the position is even legal.
+
+    #[test]
+    fn white_pawn_take_left() {
+        let b = Board::from_fen("rnbqkbnr/pppp1ppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1");
+        assert_eq!(b.pseudo_legal_moves(4, 5, MoveType::Attack), vec![(5, 4)]);
+    }
+
+    #[test]
+    fn white_pawn_not_take_center() {
+        let b = Board::from_fen("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1");
+        assert_eq!(b.pseudo_legal_moves(4, 5, MoveType::Attack), vec![]);
+    }
+
+    #[test]
+    fn white_pawn_take_right() {
+        let b = Board::from_fen("rnbqkbnr/pppp1ppp/8/5p2/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1");
+        assert_eq!(b.pseudo_legal_moves(4, 5, MoveType::Attack), vec![(5, 6)]);
+    }
+
+    #[test]
+    fn black_pawn_take_left() {
+        let b = Board::from_fen("rnbqkbnr/pppp1ppp/8/4p3/3P4/8/PPP1PPPP/RNBQKBNR w KQkq - 0 1");
+        assert_eq!(b.pseudo_legal_moves(5, 5, MoveType::Attack), vec![(6, 4)]);
+    }
+
+    #[test]
+    fn black_pawn_not_take_center() {
+        let b = Board::from_fen("rnbqkbnr/ppp1pppp/8/3p4/3P4/8/PPP1PPPP/RNBQKBNR w KQkq - 0 1");
+    }
+
+    #[test]
+    fn black_pawn_take_right() {
+        let b = Board::from_fen("rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1");
+        assert_eq!(b.pseudo_legal_moves(5, 4, MoveType::Attack), vec![(6, 5)]);
+    }
+
+    #[test]
+    fn white_pawn_double_push() {
+        let b = Board::new();
+        assert_eq!(
+            b.pseudo_legal_moves(2, 1, MoveType::Default),
+            vec![(7, 1), (6, 1)]
+        );
+    }
+
+    #[test]
+    fn black_pawn_double_push() {
+        let b = Board::new();
+        assert_eq!(
+            b.pseudo_legal_moves(7, 1, MoveType::Default),
+            vec![(4, 1), (5, 1)]
+        );
+    }
+
+    #[test]
+    fn white_pawn_double_push_blocked() {
+        let b = Board::from_fen("rnbqkbnr/1ppppppp/8/8/p7/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        assert_eq!(b.pseudo_legal_moves(2, 1, MoveType::Default), vec![(7, 1)]);
+    }
+
+    #[test]
+    fn black_pawn_double_push_blocked() {
+        let b = Board::from_fen("rnbqkbnr/pppppppp/8/P7/8/8/1PPPPPPP/RNBQKBNR w KQkq - 0 1");
+        assert_eq!(b.pseudo_legal_moves(7, 1, MoveType::Default), vec![(4, 1)]);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,28 @@
 use chess::*;
 use text_io::read;
 fn main() {
-    let mut b = Board::new();
-    b.draw();
-    for y in 0..12 {
-        for x in 0..10 {
-            println!("{}, {}: {:?}", y, x, b.pseudo_legal_moves(y, x));
+    /*let mut b = Board::new();
+        b.draw();
+        for rank in 1..=8 {
+            for file in 1..=8 {
+                println!(
+                    "{}, {}: {:?}",
+                    rank,
+                    file,
+                    b.pseudo_legal_moves(rank, file, MoveType::Attack)
+                );
+            }
         }
-    }
 
-    b.move_by_str("a2a4");
-    b.move_by_str("a1a3");
-    b.move_by_str("a3g3");
-    b.move_by_str("g3g4");
-    b.move_by_str("g4b4");
-    b.move_by_str("a4a5");
-    b.move_by_str("a5a6");
-    b.move_by_str("a6a7");
-
+        b.move_by_str("a2a4");
+        b.move_by_str("a1a3");
+        b.move_by_str("a3g3");
+        b.move_by_str("g3g4");
+        b.move_by_str("g4b4");
+        b.move_by_str("a4a5");
+        b.move_by_str("a5a6");
+        b.move_by_str("a6a7");
+    */
     /*b.move_by_str("e7e5");
     b.move_by_str("g1f3");
     b.move_by_str("b8b6");*/
